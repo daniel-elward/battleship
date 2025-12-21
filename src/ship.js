6efa4dd319483c7054ebfd32754c5a97dfb0e7c1
Daniel Elward <daniel.elward@hotmail.com>
2025-12-21 19:11:19 -0400

    added name to ship object

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,6 @@
 class CreateShip {
-    constructor(length){
+    constructor(length, name){
+        this.name = name;
         this.length = length;
         this.hits = 0;
         this.sunk = false;
