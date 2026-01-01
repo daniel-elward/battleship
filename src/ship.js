93b946a5311f4817e09d7c05dd6dd52e93a36850
Daniel Elward <daniel.elward@hotmail.com>
2025-12-31 23:12:57 -0400

    added ID field to the ship class

--- src/ship.js
+++ src/ship.js
@@ -1,9 +1,10 @@
 class CreateShip {
-    constructor(length, type){
+    constructor(length, type, id){
         this.type = type;
         this.length = length;
         this.hits = 0;
         this.sunk = false;
+        this.id = id; //manually set a unique ID each time 
     };
 
     hit(){
